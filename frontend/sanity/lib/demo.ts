33dfe2cb7a9cd5d53a3934e993cb77f2ade09b6b
douglas peng <donkiewill@gmail.com>
2025-08-31 16:02:39 -0700

    change title

--- frontend/sanity/lib/demo.ts
+++ frontend/sanity/lib/demo.ts
@@ -2,7 +2,7 @@
  * Demo data used as placeholders and initial values for the blog
  */
 
-export const title = 'Sanity + Next.js'
+export const title = 'Moto Soul Crew'
 
 export const description = [
   {
